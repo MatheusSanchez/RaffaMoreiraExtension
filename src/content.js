910de085134e226ddacf265262eadb82a62ebc74
Matheus Sanchez <matheus2.sanchez@usp.br>
2018-09-03 16:24:46 -0300

    Jquery adicionado e audio funcionando parcialmente. As fotos foram comentadas para teste BROOOOOOOOOOOOOOOOOOOO !

--- src/content.js
+++ src/content.js
@@ -1,4 +1,4 @@
-console.log("Raffa Moreira Extension Start Bro !");
+/*console.log("Raffa Moreira Extension Start Bro !");
 
 let p = document.getElementsByTagName('p'); // pega todas as tags p
 
@@ -26,3 +26,47 @@ for (ele of all_imgs) { // para todas imagens do site
 	ele.src = url; // troca o source da img
 	//console.log(url);
 }
+
+
+
+
+*/
+
+function maisAlto(myAudio){ // uma função pro range não estourar [0,1]	
+	if(myAudio.volume < 1){
+		myAudio.volume += 0.1;
+		console.log(myAudio.volume);
+	}	
+}
+
+$(document).ready(function(){
+
+	var myAudio = document.createElement("AUDIO"); // inserindo um audio no html
+	//myAudio.setAttribute("controls", "controls"); 
+	myAudio.setAttribute("src",chrome.extension.getURL('audios/fazsol.mp3')); // unico audio na pasta (ainda hahahaha)
+	
+
+	myAudio.muted = true; // tratar tambem o caso do block do chrome,
+							// a partir de uma versão ele bloqueia os autoplay :(
+	myAudio.muted = false;
+
+	myAudio.volume = 0.1;
+	myAudio.play();
+
+
+   $("*").click(function(){ // todo elemento (é lento procurar outro modo) // pegando mais de um elemento, isso faz 
+   																			//	com que o range [0,1] estoure, concertar
+
+
+    	if(myAudio.paused){ // se o audio ta pausado 
+    		console.log("Audio Tocando");
+    		myAudio.play(); // play
+    		maisAlto(myAudio);
+    	}else{
+    		console.log("Audio Pausado");
+    		myAudio.pause(); // pause
+    	} 	
+
+    });
+
+});	
